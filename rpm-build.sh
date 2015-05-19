a5aba31727d82eb2eebf51fa37115cab6511dcad
Elan Ruusamäe <glen@delfi.ee>
2015-05-19 11:13:46 +0300

    drop duplicate "ac-tag" alias, provided by rpm-build-tools

--- rpm-build.sh
+++ rpm-build.sh
@@ -15,7 +15,6 @@ if [ "$dist" ]; then
 
 alias ipoldek-$dist="poldek -q --sn $dist --cmd"
 alias $dist-provides="ipoldek-$dist what-provides"
-alias $dist-tag="./builder -cf -T $(echo $dist | tr '[a-z]' '[A-Z]')-branch -r HEAD"
 alias $dist-verify=dist-verify
 alias $dist-requires=dist-requires
 
