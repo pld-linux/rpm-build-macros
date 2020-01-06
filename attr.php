648e21aa103da55aff8a64c8a55357a23b6189b6
Jan Rękorajski <baggins@pld-linux.org>
2020-01-06 21:13:13 +0900

    - use macros defined by rpm5

--- attr.php
+++ attr.php
@@ -1,5 +1,5 @@
-%__php_provides		%{_rpmconfigdir}/php.prov
+%__php_provides		%{_rpmlibdir}/php.prov
 # define 'php_req_new' in ~/.rpmmacros to use php version of req finder
-%__php_requires		env PHP_MIN_VERSION=%{?php_min_version} %{_rpmconfigdir}/php.req%{?php_req_new:.php}
+%__php_requires		env PHP_MIN_VERSION=%{?php_min_version} %{_rpmlibdir}/php.req%{?php_req_new:.php}
 %__php_magic		^PHP script.*
 %__php_path		\\.php$
